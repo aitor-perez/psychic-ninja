1b5df497f823fffefd4fdd8fb4176694aeaf0623
Aitor Pérez <elinsidiomate@gmail.com>
2014-12-22 00:14:04 +0100

    enumeration feta i comencada la segona part

--- code/apps/polytope/src/gale_complexity.cc
+++ code/apps/polytope/src/gale_complexity.cc
@@ -18,38 +18,33 @@
 #include "polymake/Matrix.h"
 #include "polymake/ListMatrix.h"
 #include "polymake/Array.h"
+//#include "polymake/linalg.h"
 
 namespace polymake { namespace polytope {
 
-void print(const Matrix<Integer> &m) {
-  for (int i=0; i<m.rows(); ++i) {
-    for (int j=0; j<m.cols(); ++j) {
-      cout << " " << m[i][j];
+class Configuration {
+  public:
+    int e;
+    int n;
+    Vector<Rational> v;
+    Matrix<Rational> m;
+    Matrix<Rational> cocircuits;
+    
+    Configuration(int _e, int _n, const Vector<Rational>& _v) {
+      e = _e;
+      n = _n;
+      v = _v;
+      m = Matrix<Rational>(e+1, n);
+      for (int j=0; j<n; ++j) {
+        m[0][j] = 1;
+        for (int i=1; i<e+1; ++i) {
+          m[i][j] = v[j*e + i];
+        }
+      }
     }
-    cout << endl;
-  }
-}
+};
 
-void print(const Matrix<Rational> &m) {
-  for (int i=0; i<m.rows(); ++i) {
-    for (int j=0; j<m.cols(); ++j) {
-      cout << " " << m[i][j];
-    }
-    cout << endl;
-  }
-}
-
-void print(const Vector<Rational> &v) {
-  for (int i=0; i<v.dim(); ++i) {
-      cout << " " << v[i];
-  }
-  cout << endl;
-}
-
-
-
-
-Matrix<Integer> enumerate_configurations(int e, int n, int m) {
+Matrix<Rational> enumerate_configurations(int e, int n, int m) {
   Matrix<Rational> equations(e+1, e*n+1);
   for (int j=0; j<e; ++j) {
     for (int i=0; i<n; ++i) {
@@ -57,10 +52,9 @@ Matrix<Integer> enumerate_configurations(int e, int n, int m) {
     }
   }
   
-  // Adding last equation v_00 = m
-  equations(e, 1) = -m;
-  
-  cerr << equations << endl;
+  // Adding last equation v_11 = m
+  equations(e, 0) = -m;
+  equations(e, 1) = 1;
   
   ListMatrix<SparseVector<Rational> > inequalities(0, e*n+1);
   
@@ -83,33 +77,92 @@ Matrix<Integer> enumerate_configurations(int e, int n, int m) {
     inequalities /= ineq;
   }
   
-  cerr << Matrix<Rational>(inequalities) << endl;
+  // Bounding box inequalities
+  for (int i=1; i<e*n+1; ++i) {
+    SparseVector<Rational> ineq(e*n+1);
+    ineq[0] = m;
+    ineq[i] = 1;
+    inequalities /= ineq;
+    ineq[i] = -1;
+    inequalities /= ineq;
+  }
+  
+  //cerr << equations << endl;
+  //cerr << Matrix<Rational>(inequalities) << endl;
   
   perl::Object P("Polytope");
   P.take("EQUATIONS") << equations;
-  P.take("INEQUALITIES") << Matrix<Rational>(inequalities);
-  //const Matrix<Integer> C = P.give("LATTICE_POINTS"); // Això no va
-  const Matrix<Integer> C = P.CallPolymakeMethod("LATTICE_POINTS"); // S'ha de fer així
+  P.take("INEQUALITIES") << inequalities;
+  
+  const Matrix<Rational> C = P.CallPolymakeMethod("LATTICE_POINTS"); // S'ha de fer així
+
+  //cerr << "**********" << endl;
+  //cerr << C << endl;
 
   return C;
 }
 
+bool check_lexicographically_sorted(int e, const Vector<Rational>& v) {
+  int n = (v.size()-1)/e;
 
-Array<Matrix<Integer> > gale_complexity(int e, int n, int m) {
-  Matrix<Integer> mat = enumerate_configurations(e,n,m);
+  int prev = 1;
+  for (int i=1; i<n; i++) {
+    int next = 1 + e*i;
+    
+    for (int j=0; j<e; ++j) {
+      if (v[prev + j] > v[next + j]) {
+        break;
+      }
+      
+      if (v[prev + j] < v[next + j]) {
+        return false;
+      }
+    }
+    
+    prev = next;
+  }
   
-  Array<Matrix<Integer> > a(1, mat);
+  return true;
+}
 
-  return a;
+Array<Matrix<Rational> > gale_complexity(int e, int n, int m) {
+  Matrix<Rational> configs = enumerate_configurations(e,n,m);
+  
+  int pos = 0;
+  int neg = 0;
+  for (int k=0; k<configs.rows(); ++k) {
+    if (check_lexicographically_sorted(e, configs[k])) {
+      ++pos;
+      
+      Configuration conf(e, n, configs[k]);
+      
+      cerr << conf.v << endl;
+      cerr << "***" << endl;
+      cerr << conf.m << endl;
+      cerr << "***" << endl;
+      
+//      Matrix<Rational> kern = T(null_space(T(conf.m)));
+      
+  //    cerr << kern << endl;
+      break;
+    } else {
+      ++neg;
+    }
+  }
+  
+  cerr << pos << "vs" << neg << endl;
+  cerr << "Total: " << pos+neg << endl;
+  
+  return Array<Matrix<Rational> > (0);
 }
 
 UserFunctionTemplate4perl("# @category Computations"
-                          "# Compute a vector that selects the inclusion-minimal face "
-			  "# that contains the vertices indexed by I."
-                          "# @param Polytope P the input polytope"
-                          "# @param Set I the indices of some vertices of P"
-                          "# @return Vector",
-			  "gale_complexity(Int, Int, Int)" );
+                          "# Computes the lattice points of all polytopes with Gale complexity m."
+                          "# @param Int e Dimension of the Gale diagram vector space"
+                          "# @param Int n Number of vectors of the Gale diagram (i.e. number of vertices of the polytopes)"
+                          "# @param Int m Gale complexity of the polytopes"
+                          "# @return Array<Matrix<Rational> > Array of all the lattice points for all the polytopes",
+                  			  "gale_complexity(Int, Int, Int)" );
 
 } }
 
